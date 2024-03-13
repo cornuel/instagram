c525edb60ab7555646b61a6065d0f324584c6a56
cornuel <ultimn8@gmail.com>
2024-03-13 17:26:02 +0100

    modified tag

--- src/components/Atom/Tag.vue
+++ src/components/Atom/Tag.vue
@@ -17,16 +17,16 @@ const props = defineProps({
 const classes = computed(() => {
   const sizeClasses = {
     small: 'text-xs px-1 m-0.5',
-    medium: 'text-sm px-1 m-0.5',
+    medium: 'text-xs font-semibold px-1 m-0.5 border',
     large: 'text-base px-1 m-1 border-[1.5px]'
   }
 
   return [
     sizeClasses[props.size] || sizeClasses.medium,
-    'rounded-full transition ease-in-out delay-80 duration-300',
-    'border border-borderColor bg-bgColor-primary ',
-    'hover:cursor-pointer',
-    'hover:border-buttonColor-primary',
+    'rounded-full transition ease-in-out delay-120 duration-300',
+    'border border-bg-bgColor-secondary hover:bg-gradient-to-r from-buttonColor-primary to-buttonColor-primary-hover ',
+    'hover:cursor-pointer hover:text-buttonColor-secondary',
+    'hover:border-buttonColor-primary hover:border',
     'hover:shadow-sm'
   ]
 })
