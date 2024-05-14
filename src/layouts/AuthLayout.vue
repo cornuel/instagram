702be49a10876bbd4c2686bd41591cbf52ba6168
cornuel <ultimn8@gmail.com>
2024-05-14 12:24:24 +0200

    Modified AuthLayout.vue for google bot

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,7 +1,17 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section class="auth flex flex-col h-screen">
+  <section class="auth flex flex-col h-screen items-center">
+    <div class="w-fit mt-5 border border-red-600 text-red-500 p-5 rounded-md">
+      This is a clone of Instagram for demonstration purposes only and it is not
+      affiliated with the original platform whatsoever.
+      <a
+        href="https://github.com/cornuel/instagram"
+        target="_blank"
+      >
+        Repo available here on my github.
+      </a>
+    </div>
     <main class="flex flex-grow w-full justify-center">
       <slot />
     </main>
